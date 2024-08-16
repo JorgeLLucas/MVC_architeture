7d04a84e2d85eed1758b835cd20d8edb11c520f7
Jorge Lucas <jorgelucas.andrade15@gmail.com>
2024-08-16 11:48:50 -0300

    Criação da ação Show dentro do controller de products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,15 @@
 class ProductsController < ApplicationController
+    before_action :set_product, only: [:show]
+    
     def index
-        @products = Product.all    
+        @products = Product.all    #uso da variavel de instancia para que todos os elementos da tabela products sejam exibidos na view products/index
+    end 
+
+    def show; end
+
+    private
+
+    def set_product
+        @product = Product.find(params[:id])
     end
 end
